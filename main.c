05f2545b6756570dbc7632697e8b11ece71ccaa9
BlackTroller <pedro.miguel.ribeiro01@gmail.com>
2022-12-09 19:13:52 +0000

    first commit

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cFiles/main.c to edit this template
+ */
+
+/* 
+ * File:   main.c
+ * Author: pedro
+ *
+ * Created on 9 de dezembro de 2022, 19:12
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
